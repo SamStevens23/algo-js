731f5c469e9db4f4c02ce68dbfeafaa8aeb66ce9
ampersam <stevens.sam2323@gmail.com>
2020-10-18 12:50:39 +0200

    ex 5.2

--- ex5.js
+++ ex5.js
@@ -10,7 +10,7 @@ const readlineSync = require("readline-sync");
 
 // for (let i = 0; i < n; i ++) { 
 
-//     let members = readlineSync.question ("Name some cast members");
+//     let members = readlineSync.question ("Name a member ");
 //     arr.push(members);
 
 // }
@@ -21,6 +21,7 @@ const readlineSync = require("readline-sync");
 //     "cast_members": arr
 // };
 
+
 // return myObject;
 
 // }
@@ -29,45 +30,58 @@ const readlineSync = require("readline-sync");
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 // EXERCISE 2
 
-// THIS IS ALEX' CODE THAT I DONT REALLY UNDERSTAND YET
+let n = readlineSync.question ("How many cast members will you choose? ");
 
-// the object created with ex 5.1
-// const readlineSync = require("readline-sync");
+let arr = [];
 
-// function askTvSerie(name, productionYear, castMembers) {
-//     this.name = readlineSync.question("Enter the NAME of your favorite TV serie \n");
-//     this.productionYear = readlineSync.question("Enter the PRODUCTION YEAR of your favorite TV serie \n");
-//     let arr = new Array;
-//     let n = new Number(readlineSync.question("how much member cast you have? "));
-//     for (let i = 0; i < n; i++) {
-//         arr.push(readlineSync.question("cast member " + (i + 1) + ": "));
+for (let i = 0; i < n; i ++) { 
 
-//     }
-//     this.castMembers = arr;
+    let members = readlineSync.question ("Name a member ");
+    arr.push(members);
 
-// }
+}
+
+// the Fisher-Yates Shuffle
+
+function shuffle(array) {
+    let counter = array.length;
+
+    while (counter > 0) {
+      
+        let index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
 
 
-// var obj1 = new askTvSerie;
+let random = shuffle(arr);
 
-// console.log(JSON.stringify(obj1));
-
-// // exercise 5.2
-// var castArray = Object.values(obj1.castMembers);
+console.log(random);
 
 
+///////////////////
 
 
-// function randomizeCast(tvSerie) {
-//     tvSerie.sort(() => Math.random() - 0.5);
-// }
 
-// randomizeCast(castArray);
 
-// console.log(JSON.stringify(castArray));
+
+
+
+
+
+
+
+
+
 
 
 
